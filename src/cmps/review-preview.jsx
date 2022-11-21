8f70875f6c46288bc6440b7222efd3ffe80acc85
Yarin <yarindayan11@gmail.com>
2022-11-21 14:07:00 +0200

    fixed changes

--- src/cmps/review-preview.jsx
+++ src/cmps/review-preview.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 
-export function ReviewPreview({ review}) {
+export function ReviewPreview({ review }) {
     console.log('asdasdasd');
     return (
         <div className="review-preview">
             <p>{review?.review}</p>
+            <div>
+                <span class={`fa fa-star ${review.stars >= 1 ? 'checked' : ''}`}></span>
+                <span class={`fa fa-star ${review.stars >= 2 ? 'checked' : ''}`}></span>
+                <span class={`fa fa-star ${review.stars >= 3 ? 'checked' : ''}`}></span>
+                <span class={`fa fa-star ${review.stars >= 4 ? 'checked' : ''}`}></span>
+                <span class={`fa fa-star ${review.stars >= 5 ? 'checked' : ''}`}></span>
+            </div>
         </div>
     )
 }
